85ffaeaf8624c283463cb1c579fb3b2ef40320a1
tharaka dissanayake <tharakasd95@gmail.com>
2023-06-11 12:14:31 +0530

    connected to the mongodb

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config({ path: "../config/.env" });
 const app = express();
 const port = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.CONNECTION_URI)
+  .then(() => console.log("connected to the mongodb "))
+  .catch((err) => console.error("coudnot connect to the mongo db " + err));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
